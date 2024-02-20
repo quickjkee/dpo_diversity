a3f8e1dac676d583f2913f43a695f5950beeafbe
quickjkee <jkee2013@mail.ru>
2024-02-20 11:09:26 +0300

    odib

--- dpo_diffusion/train_diffusion_dpo.py
+++ dpo_diffusion/train_diffusion_dpo.py
@@ -932,8 +932,8 @@ def main(args):
                     labels = model_ours.classify(img_1_batch, img_2_batch)
                     labels = -1 * (labels * 2 - 1)  # [0, 1] -> [-1, 1] -> [1, -1]
 
-                scores = model_losses_w + model_losses_l
-                logits_div = F.logsigmoid(scores) * labels
+                scores = model_losses_w + model_losses_l - ref_losses_w - ref_losses_l
+                logits_div = F.logsigmoid(args.beta_dpo * scores) * labels
                 loss_div = -1 * (logits_div.mean())
                 ############################
 
