f72e9cb1348b62d4c6e5d0cbe4b899e6a4ac49bc
quickjkee <jkee2013@mail.ru>
2024-02-22 17:08:27 +0300

    odib

--- dpo_diffusion/train_diffusion_dpo.py
+++ dpo_diffusion/train_diffusion_dpo.py
@@ -602,7 +602,7 @@ def main(args):
 
     # PICKSCORE
     ######################
-    model_pick = AutoModel.from_pretrained(args.clip_model_name_or_path).eval().to(accelerator.device)
+    model_pick = AutoModel.from_pretrained(args.pickscore_model_name_or_path).eval().to(accelerator.device)
     ######################
 
     vae.requires_grad_(False)
@@ -779,13 +779,11 @@ def main(args):
             image_1_embs = model_pick.get_image_features(img_1_batch.to(accelerator.device))
             image_1_embs = image_1_embs / torch.norm(image_1_embs, dim=-1, keepdim=True)
             score_1 = (text_embs * image_1_embs).sum(-1)
-            print(score_1)
             mask_1 = (score_1 > q_value) * 1.0
 
             image_2_embs = model_pick.get_image_features(img_2_batch.to(accelerator.device))
             image_2_embs = image_2_embs / torch.norm(image_2_embs, dim=-1, keepdim=True)
             score_2 = (text_embs * image_2_embs).sum(-1)
-            print(score_2)
             mask_2 = (score_2 > q_value) * 1.0
 
             mask = mask_1 * mask_2
